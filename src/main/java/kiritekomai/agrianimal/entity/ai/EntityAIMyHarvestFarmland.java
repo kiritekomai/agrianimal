62e27dd490b24dc8c7a5a1e2fa98bd9c5710c5f6
kiritekomai <under.dog.1v6a81Z@gmail.com>
2019-04-10 20:25:33 +0900

    Add entity base class and related AI

---
+++ src/main/java/kiritekomai/agrianimal/entity/ai/EntityAIMyHarvestFarmland.java
@@ -0,0 +1,188 @@
+package kiritekomai.agrianimal.entity.ai;
+
+import kiritekomai.agrianimal.entity.EntityAgriAnimal;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockAttachedStem;
+import net.minecraft.block.BlockCrops;
+import net.minecraft.block.BlockHorizontal;
+import net.minecraft.block.BlockReed;
+import net.minecraft.block.BlockStemGrown;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.ai.EntityAIMoveToBlock;
+import net.minecraft.init.Blocks;
+import net.minecraft.init.Items;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorld;
+import net.minecraft.world.IWorldReaderBase;
+
+public class EntityAIMyHarvestFarmland extends EntityAIMoveToBlock {
+	/** Villager that is harvesting */
+	private final EntityAgriAnimal agriAnimal;
+	private boolean hasFarmItem;
+	/** 0 => harvest, 1 => replant, -1 => none */
+	private int currentTask;
+
+	public EntityAIMyHarvestFarmland(EntityAgriAnimal farmerIn, double speedIn) {
+		super(farmerIn, speedIn, 16);
+		this.agriAnimal = farmerIn;
+	}
+
+	/**
+	 * Returns whether the EntityAIBase should begin execution.
+	 */
+	public boolean shouldExecute() {
+		if (!this.agriAnimal.isHarvesting()) {
+			return false;
+		}
+		if (this.runDelay <= 0) {
+			if (!net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.agriAnimal.world,
+					this.agriAnimal)) {
+				return false;
+			}
+
+			this.currentTask = -1;
+			this.hasFarmItem = this.agriAnimal.isFarmItemInInventory();
+		}
+
+		return super.shouldExecute();
+	}
+
+	/**
+	 * Returns whether an in-progress EntityAIBase should continue executing
+	 */
+	public boolean shouldContinueExecuting() {
+		return this.currentTask >= 0 && super.shouldContinueExecuting();
+	}
+
+	/**
+	 * Keep ticking a continuous task that has already been started
+	 */
+	public void tick() {
+		super.tick();
+		this.agriAnimal.getLookHelper().setLookPosition((double) this.destinationBlock.getX() + 0.5D,
+				(double) (this.destinationBlock.getY() + 1), (double) this.destinationBlock.getZ() + 0.5D, 10.0F,
+				(float) this.agriAnimal.getVerticalFaceSpeed());
+		if (this.getIsAboveDestination()) {
+			IWorld iworld = this.agriAnimal.world;
+			BlockPos blockpos = this.destinationBlock.up();
+			IBlockState iblockstate = iworld.getBlockState(blockpos);
+			Block block = iblockstate.getBlock();
+			if (this.currentTask == 0 && block instanceof BlockCrops && ((BlockCrops) block).isMaxAge(iblockstate)) {
+				iworld.destroyBlock(blockpos, true);
+			} else if (this.currentTask == 0 && getStemGlownBlockPos(iworld, blockpos) != null) {
+				iworld.destroyBlock(getStemGlownBlockPos(iworld, blockpos), true);
+			} else if (this.currentTask == 0 && getBlockReedPos(iworld, blockpos) != null) {
+				iworld.destroyBlock(getBlockReedPos(iworld, blockpos), true);
+			} else if (this.currentTask == 1 && iblockstate.isAir()) {
+				InventoryBasic inventorybasic = this.agriAnimal.getInventory();
+
+				for (int i = 0; i < inventorybasic.getSizeInventory(); ++i) {
+					ItemStack itemstack = inventorybasic.getStackInSlot(i);
+					boolean flag = false;
+					if (!itemstack.isEmpty()) {
+						if (itemstack.getItem() == Items.WHEAT_SEEDS) {
+							iworld.setBlockState(blockpos, Blocks.WHEAT.getDefaultState(), 3);
+							flag = true;
+						} else if (itemstack.getItem() == Items.POTATO) {
+							iworld.setBlockState(blockpos, Blocks.POTATOES.getDefaultState(), 3);
+							flag = true;
+						} else if (itemstack.getItem() == Items.CARROT) {
+							iworld.setBlockState(blockpos, Blocks.CARROTS.getDefaultState(), 3);
+							flag = true;
+						} else if (itemstack.getItem() == Items.BEETROOT_SEEDS) {
+							iworld.setBlockState(blockpos, Blocks.BEETROOTS.getDefaultState(), 3);
+							flag = true;
+						} else if (itemstack.getItem() instanceof net.minecraftforge.common.IPlantable) {
+							if (((net.minecraftforge.common.IPlantable) itemstack.getItem()).getPlantType(iworld,
+									blockpos) == net.minecraftforge.common.EnumPlantType.Crop) {
+								iworld.setBlockState(blockpos,
+										((net.minecraftforge.common.IPlantable) itemstack.getItem()).getPlant(iworld,
+												blockpos),
+										3);
+								flag = true;
+							}
+						}
+					}
+
+					if (flag) {
+						itemstack.shrink(1);
+						if (itemstack.isEmpty()) {
+							inventorybasic.setInventorySlotContents(i, ItemStack.EMPTY);
+						}
+						break;
+					}
+				}
+			}
+
+			this.currentTask = -1;
+			this.runDelay = 10;
+		}
+
+	}
+
+	/**
+	 * Return true to set given position as destination
+	 */
+	protected boolean shouldMoveTo(IWorldReaderBase worldIn, BlockPos pos) {
+		Block block = worldIn.getBlockState(pos).getBlock();
+		pos = pos.up();
+		if (block == Blocks.FARMLAND) {
+			IBlockState iblockstate = worldIn.getBlockState(pos);
+			block = iblockstate.getBlock();
+			if (block instanceof BlockCrops && ((BlockCrops) block).isMaxAge(iblockstate)
+					&& (this.currentTask == 0 || this.currentTask < 0)) {
+				this.currentTask = 0;
+				return true;
+			}
+
+			if (iblockstate.isAir() && this.hasFarmItem && (this.currentTask == 1 || this.currentTask < 0)) {
+				this.currentTask = 1;
+				return true;
+			}
+		}
+		if ((getStemGlownBlockPos(worldIn, pos) != null)
+				&& (this.currentTask == 0 || this.currentTask < 0)) {
+			this.currentTask = 0;
+			return true;
+
+		}
+		if ((getBlockReedPos(worldIn, pos) != null)
+				&& (this.currentTask == 0 || this.currentTask < 0)) {
+			this.currentTask = 0;
+			return true;
+
+		}
+
+		return false;
+	}
+
+	protected BlockPos getStemGlownBlockPos(IWorldReaderBase worldIn, BlockPos pos) {
+		Block block = worldIn.getBlockState(pos).getBlock();
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+
+		if (block instanceof BlockAttachedStem) {
+			BlockPos facing_pos = pos.offset(iblockstate.get(BlockHorizontal.HORIZONTAL_FACING));
+			Block facing_block = worldIn.getBlockState(facing_pos).getBlock();
+			if (facing_block instanceof BlockStemGrown) {
+				return facing_pos;
+			}
+		}
+		return null;
+	}
+
+	protected BlockPos getBlockReedPos(IWorldReaderBase worldIn, BlockPos pos) {
+		Block block = worldIn.getBlockState(pos).getBlock();
+
+		if (block instanceof BlockReed) {
+			BlockPos up_pos = pos.up();
+			Block up_block = worldIn.getBlockState(up_pos).getBlock();
+			if (up_block instanceof BlockReed) {
+				return up_pos;
+			}
+		}
+		return null;
+	}
+
+}
---
+++ src/main/java/kiritekomai/agrianimal/entity/ai/EntityAIPutItemInChest.java
@@ -0,0 +1,243 @@
+package kiritekomai.agrianimal.entity.ai;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import kiritekomai.agrianimal.entity.EntityAgriAnimal;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockChest;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.ai.EntityAIMoveToBlock;
+import net.minecraft.inventory.IInventory;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.EntitySelectors;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IWorldReaderBase;
+import net.minecraft.world.World;
+
+public class EntityAIPutItemInChest extends EntityAIMoveToBlock {
+	/** Villager that is harvesting */
+	private final EntityAgriAnimal agriAnimal;
+	private boolean isEntityInventoryFull;
+	/** 0 => harvest, 1 => replant, -1 => none */
+	private int currentTask;
+
+	public EntityAIPutItemInChest(EntityAgriAnimal farmerIn, double speedIn) {
+		super(farmerIn, speedIn, 16);
+		this.agriAnimal = farmerIn;
+	}
+
+	/**
+	 * Returns whether the EntityAIBase should begin execution.
+	 */
+	public boolean shouldExecute() {
+		if (this.runDelay <= 0) {
+			if (!net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.agriAnimal.world,
+					this.agriAnimal)) {
+				return false;
+			}
+
+			this.currentTask = -1;
+			this.isEntityInventoryFull = isInventoryFull((IInventory) this.agriAnimal.getInventory());
+		}
+
+		return super.shouldExecute();
+	}
+
+	/**
+	 * Returns whether an in-progress EntityAIBase should continue executing
+	 */
+	public boolean shouldContinueExecuting() {
+		return this.currentTask >= 0 && super.shouldContinueExecuting();
+	}
+
+	/**
+	 * Keep ticking a continuous task that has already been started
+	 */
+	public void tick() {
+		super.tick();
+		this.agriAnimal.getLookHelper().setLookPosition((double) this.destinationBlock.getX() + 0.5D,
+				(double) (this.destinationBlock.getY() + 0.5D), (double) this.destinationBlock.getZ() + 0.5D, 10.0F,
+				(float) this.agriAnimal.getVerticalFaceSpeed());
+		if (this.getIsAboveDestination()) {
+			World world = this.agriAnimal.world;
+			IInventory iinventory = getInventoryAtPosition(world, this.destinationBlock);
+
+			//iinventory.openInventory(null);
+			this.transferItems((IInventory) this.agriAnimal.getInventory(), iinventory);
+			//iinventory.closeInventory(null);
+
+			this.currentTask = -1;
+			this.runDelay = 10;
+		}
+
+	}
+
+	/**
+	 * Return true to set given position as destination
+	 */
+	protected boolean shouldMoveTo(IWorldReaderBase worldIn, BlockPos pos) {
+		if (!isEntityInventoryFull) {
+			//Don't move while putting items or has inventry space
+			return false;
+		}
+		IInventory iinventory = getInventoryAtPosition((World) worldIn, pos);
+		if (iinventory == null) {
+			//Don't move if block isn't chest
+			return false;
+		}
+
+		if (!isInventoryFull(iinventory) && isInventoryFull((IInventory) this.agriAnimal.getInventory())) {
+			this.currentTask = 1;
+			return true;
+		}
+		return false;
+	}
+
+	@Nullable
+	public static IInventory getInventoryAtPosition(World worldIn, BlockPos pos) {
+		IInventory iinventory = null;
+		IBlockState iblockstate = worldIn.getBlockState(pos);
+		Block block = iblockstate.getBlock();
+		if (iblockstate.hasTileEntity()) {
+			TileEntity tileentity = worldIn.getTileEntity(pos);
+			if (tileentity instanceof IInventory) {
+				iinventory = (IInventory) tileentity;
+				if (iinventory instanceof TileEntityChest && block instanceof BlockChest) {
+					iinventory = ((BlockChest) block).getContainer(iblockstate, worldIn, pos, true);
+				}
+			}
+		}
+
+		if (iinventory == null) {
+			List<Entity> list = ((World) worldIn).getEntitiesInAABBexcluding((Entity) null, new AxisAlignedBB(pos),
+					EntitySelectors.HAS_INVENTORY);
+			if (!list.isEmpty()) {
+				iinventory = (IInventory) list.get(worldIn.rand.nextInt(list.size()));
+			}
+		}
+
+		return iinventory;
+	}
+
+	/**
+	 * Returns false if the inventory has any room to place items in
+	 */
+	private boolean isInventoryFull(IInventory inventoryIn) {
+		int i = inventoryIn.getSizeInventory();
+
+		for (int j = 0; j < i; ++j) {
+			ItemStack itemstack = inventoryIn.getStackInSlot(j);
+			if (itemstack.isEmpty()) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+	private void transferItems(IInventory iinventoryFrom, IInventory iinventoryTo) {
+		//if (net.minecraftforge.items.VanillaInventoryCodeHooks.insertHook(this)) return;
+		if (iinventoryFrom == null || iinventoryTo == null) {
+			return;
+		}
+		if (this.isInventoryFull(iinventoryTo)) {
+			return;
+		}
+		for (int i = 0; i < iinventoryFrom.getSizeInventory(); ++i) {
+			if (!iinventoryFrom.getStackInSlot(i).isEmpty()) {
+				ItemStack itemstack = iinventoryFrom.getStackInSlot(i).copy();
+				Item item = itemstack.getItem();
+				boolean needPut = false;
+				if (!this.agriAnimal.isFarmableItem(item)) {
+					needPut = true;
+				} else {
+					for (int j = 0; j < i; j++) {
+						if (item == iinventoryFrom.getStackInSlot(j).getItem()) {
+							needPut = true;
+							break;
+						}
+					}
+				}
+				if (needPut) {
+					ItemStack itemstack1 = putStackInInventoryAllSlots(iinventoryFrom, iinventoryTo, itemstack);
+					iinventoryFrom.setInventorySlotContents(i, itemstack1);
+					if (itemstack1.isEmpty()) {
+						iinventoryFrom.markDirty();
+					}
+				}
+			}
+		}
+
+	}
+
+	/**
+	 * Attempts to place the passed stack in the inventory, using as many slots as required. Returns leftover items
+	 */
+	public static ItemStack putStackInInventoryAllSlots(@Nullable IInventory source, IInventory destination,
+			ItemStack stack) {
+		int i = destination.getSizeInventory();
+
+		for (int j = 0; j < i && !stack.isEmpty(); ++j) {
+			stack = insertStack(source, destination, stack, j);
+		}
+
+		return stack;
+	}
+
+	/**
+	 * Insert the specified stack to the specified inventory and return any leftover items
+	 */
+	private static ItemStack insertStack(@Nullable IInventory source, IInventory destination, ItemStack stack,
+			int index) {
+		ItemStack itemstack = destination.getStackInSlot(index);
+		if (canInsertItemInSlot(destination, stack, index)) {
+			boolean flag = false;
+			if (itemstack.isEmpty()) {
+				destination.setInventorySlotContents(index, stack);
+				stack = ItemStack.EMPTY;
+				flag = true;
+			} else if (canCombine(itemstack, stack)) {
+				int i = stack.getMaxStackSize() - itemstack.getCount();
+				int j = Math.min(stack.getCount(), i);
+				stack.shrink(j);
+				itemstack.grow(j);
+				flag = j > 0;
+			}
+
+			if (flag) {
+				destination.markDirty();
+			}
+		}
+
+		return stack;
+	}
+
+	/**
+	 * Can this hopper insert the specified item from the specified slot on the specified side?
+	 */
+	private static boolean canInsertItemInSlot(IInventory inventoryIn, ItemStack stack, int index) {
+		if (!inventoryIn.isItemValidForSlot(index, stack)) {
+			return false;
+		}
+		return true;
+	}
+
+	private static boolean canCombine(ItemStack stack1, ItemStack stack2) {
+		if (stack1.getItem() != stack2.getItem()) {
+			return false;
+		} else if (stack1.getDamage() != stack2.getDamage()) {
+			return false;
+		} else if (stack1.getCount() > stack1.getMaxStackSize()) {
+			return false;
+		} else {
+			return ItemStack.areItemStackTagsEqual(stack1, stack2);
+		}
+	}
+}
